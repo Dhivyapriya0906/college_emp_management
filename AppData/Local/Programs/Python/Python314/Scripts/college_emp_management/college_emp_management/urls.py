ed8782db8525248ca10703a6799663634328716a
DHIVYAPRIYA <indiradhivyapriya@gmail.com>
2026-07-25 20:48:11 +0530

    Delete AppData/Local/Programs/Python/Python314/Scripts/college_emp_management directory

--- AppData/Local/Programs/Python/Python314/Scripts/college_emp_management/college_emp_management/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("", include("dashboard.urls")),   # Home page
-    path("admin/", admin.site.urls),
-]
